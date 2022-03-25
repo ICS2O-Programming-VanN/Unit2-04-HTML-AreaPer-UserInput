0a0552d19f52546a29b51ddf57bdd203da37c1b1
Van Nguyen <99274250+van-nguyen2006@users.noreply.github.com>
2022-03-25 17:37:29 +0000

    Added Favicon, color, input and output with JS function, centered DIV, added an image.

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates the area of a triangle.
+ */
+function calculate () {
+  // input
+  const height = parseFloat(document.getElementById('height-of-triangle').value)
+  const base = parseFloat(document.getElementById('base-of-triangle').value)
+
+  // process
+  const area = (height * base) / 2
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + area + ' cm²'
+}
