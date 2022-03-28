6389fddd584b676b8f47f612b39ff337896e75ec
Van Nguyen <99274250+van-nguyen2006@users.noreply.github.com>
2022-03-28 21:20:42 +0000

    Added more comments.

--- js/script.js
+++ js/script.js
@@ -5,12 +5,12 @@
  * This function calculates the area of a triangle.
  */
 function calculate () {
-  // input
-  const height = parseFloat(document.getElementById('height-of-triangle').value)
-  const base = parseFloat(document.getElementById('base-of-triangle').value)
+  // input makes value a float
+  let height = parseFloat(document.getElementById('height-of-triangle').value)
+  let base = parseFloat(document.getElementById('base-of-triangle').value)
 
-  // process
-  const area = (height * base) / 2
+  // process (formula for area)
+  let area = (height * base) / 2
 
   // output
   document.getElementById('area').innerHTML = 'Area is: ' + area + ' cm²'
